e15922e80b3117936e3a16083c01d42816ce246f
Patrick Nodder <patrick.nodder@tpicap.com>
2018-02-26 17:39:56 +0000

    add admin user and page

---
+++ src/main/java/pnodder/controllers/AdminController.java
@@ -0,0 +1,17 @@
+package pnodder.controllers;
+
+import org.apache.shiro.SecurityUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/admin")
+public class AdminController {
+    
+    @GetMapping
+    public String admin() {
+        if (SecurityUtils.getSubject().isAuthenticated()
+                && )
+    }
+    
+    
+}
